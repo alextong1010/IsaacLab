9deee99896723a3104f1ac64fc7ec934973a781a
Alex Tong <yifantong1010@gmail.com>
2025-02-26 15:46:47 -0500

    init

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
@@ -54,3 +54,49 @@ gym.register(
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
 )
+
+# Register custom environments
+gym.register(
+    id="Custom-Isaac-Velocity-Flat-Unitree-Go2-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.custom_flat_env_cfg:CustomUnitreeGo2FlatEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.custom_rsl_rl_ppo_cfg:CustomUnitreeGo2FlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Custom-Isaac-Velocity-Flat-Unitree-Go2-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.custom_flat_env_cfg:CustomUnitreeGo2FlatEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.custom_rsl_rl_ppo_cfg:CustomUnitreeGo2FlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+    },
+)
+
+# Register custom rough environments
+gym.register(
+    id="Custom-Isaac-Velocity-Rough-Unitree-Go2-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.custom_rough_env_cfg:CustomUnitreeGo2RoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.custom_rsl_rl_ppo_cfg:CustomUnitreeGo2RoughPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Custom-Isaac-Velocity-Rough-Unitree-Go2-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.custom_rough_env_cfg:CustomUnitreeGo2RoughEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.custom_rsl_rl_ppo_cfg:CustomUnitreeGo2RoughPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
+    },
+)
---
+++ train_rsl_rl_go2_flat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Change to workflow to be one of {rsl_rl, skrl, sb3, or rl_games}
+# Unitree Go2 env officially supports rsl_rl and skrl
+SCRIPT_PATH="scripts/reinforcement_learning/rsl_rl/train.py"
+TASK="Custom-Isaac-Velocity-Flat-Unitree-Go2-v0"
+NUM_ENVS="4096"
+MAX_ITERATIONS="10000"
+SEED="100"
+VIDEO_LENGTH="200" # in steps
+VIDEO_INTERVAL="2000" # in steps
+
+# Use this to toggle which cuda device gets used for training
+export CUDA_VISIBLE_DEVICES=0
+export HYDRA_FULL_ERROR=1
+
+
+# Run the command with specified arguments
+./isaaclab.sh -p "$SCRIPT_PATH" --task "$TASK" --num_envs "$NUM_ENVS" --seed "$SEED" --headless --max_iterations "$MAX_ITERATIONS" --video --video_length "$VIDEO_LENGTH" --video_interval "$VIDEO_INTERVAL"
